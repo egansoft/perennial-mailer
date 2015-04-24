5ebbf1868847893783c6d01c3234748d65732619
Nicholas Egan <nicholas@egansoft.com>
2015-04-23 21:49:18 -0700

    Made most of it probably

---
+++ index.js
@@ -0,0 +1,27 @@
+var settings = require('/.settings')
+var nodemailer = require('nodemailer')
+
+var transporter = nodemailer.createTransport({
+    service: settings.fromService,
+    auth: {
+        user: settings.fromEmail,
+        pass: settings.fromPassword
+    }
+})
+
+var mailOptions = {
+    from: settings.fromEmail,
+    to: toEmail.join(', '),
+}
+
+setInterval(function() {
+    mailOptions.subject = '[perennial] ' + Date.now()
+    mailOptions.text = 'This is just another email from perennial mailer, sent at ' + Date.now()
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        if(error)
+            console.log(error)
+        else
+            console.log('Message sent: ' + info.response)
+    })
+}, settings.interval)
